cc485b5adc3c858be364ecff5ca07b73c3330260
Alex <a.schaaf@abdn.ac.uk>
2019-04-18 14:03:27 +0100

    linting, typing, streamlining

--- gio/grids.py
+++ gio/grids.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from nptyping import Array
 
 
 def read_irap_classic_grid(fp):
@@ -28,7 +29,8 @@ def read_irap_classic_grid(fp):
             z_values.append(float(z_value))
     z_values = np.array(z_values)
 
-    Z_grid = np.array([z_values[(i - 1) * nx:i * nx] for i in range(1, ny + 1)])
+    Z_grid = np.array(
+        [z_values[(i - 1) * nx:i * nx] for i in range(1, ny + 1)])
     y_coords = np.arange(ny) * gy + extent[0]
     x_coords = np.arange(nx) * gx + extent[2]
 
@@ -39,7 +41,7 @@ def read_irap_classic_grid(fp):
     return points[boolean_filter, :]
 
 
-def read_cps3(fp:str, drop_empty:bool=True, return_grid:bool=False):
+def read_cps3(fp: str, drop_empty: bool = True, return_grid: bool = False):
     """Read CPS-3 gridded regular surface files exported by Petrel 2017 and
     returns a Pandas DataFrame.
 
@@ -69,8 +71,8 @@ def read_cps3(fp:str, drop_empty:bool=True, return_grid:bool=False):
     de = np.arange(0, len(grid) + 1, len(grid) / fsnrow[1]).astype(int)
     for aa, bb in zip(de[:-1], de[1:]):
         row = grid[aa:bb]
-        flat_row = np.array([item for sublist in
-                             row for item in sublist]).astype(float)
+        flat_row = np.array([item for sublist in row
+                             for item in sublist]).astype(float)
         rows.append((flat_row))
 
     rows = np.array(rows)
@@ -91,26 +93,29 @@ def read_cps3(fp:str, drop_empty:bool=True, return_grid:bool=False):
     if drop_empty:
         g = g[:, g[2, :] != np.max(g)]
 
-    df = pd.DataFrame(columns=['X', 'Y', 'Z'])
-    df["X"] = g[0,:]
+    df = pd.DataFrame(columns=["X", "Y", "Z"])
+    df["X"] = g[0, :]
     df["Y"] = g[1, :]
     df["Z"] = g[2, :]
 
     return df
 
 
-def read_earth_vision_grid(fp:str,
-                           surface:str=None,
-                           preserve_colrow:bool=False,
-                           group:str=None):
+def read_earth_vision_grid(fp: str,
+                           surface: str = None,
+                           preserve_colrow: bool = False,
+                           group: str = None) -> pd.DataFrame:
     """
-    Reads Earth Vision Grid files exported by Petrel into GemPy Interfaces-compatible DataFrame.
+    Reads Earth Vision Grid files exported by Petrel into GemPy 
+    Interfaces-compatible DataFrame.
 
     Args:
         fp (str): Filepath, e.g. "/surfaces/layer1"
         surface (str): Formation name, Default None
-        preserve_colrow (bool): If True preserves row and column values saved in the Earth Vision Grid file. Default False
-        group (str): If given creates columns with a group name (useful to later identification of formation subsets). Default None
+        preserve_colrow (bool): If True preserves row and column values saved
+            in the Earth Vision Grid file. Default False
+        group (str): If given creates columns with a group name (useful to 
+            later identification of formation subsets). Default None
 
     Returns:
         pandas.DataFrame
@@ -124,8 +129,8 @@ def read_earth_vision_grid(fp:str,
     df["formation"] = surface
 
     if not preserve_colrow:
-        df.drop('col', axis=1, inplace=True)
-        df.drop('row', axis=1, inplace=True)
+        df.drop("col", axis=1, inplace=True)
+        df.drop("row", axis=1, inplace=True)
 
     if group:
         df["group"] = group
@@ -133,30 +138,47 @@ def read_earth_vision_grid(fp:str,
     return df
 
 
-def get_orient(simplices, points):
+def get_orient(vertices, simplices) -> tuple:
+    """Calculates triangle centroids and normal vector from given simplices and
+    points.
+
+    Args:
+        simplices (np.ndarray): Array (n, 3) containing indices of trianges.
+        vertices (np.ndarray): [description]
+
+    Returns:
+        tuple: [0] triangle normals Array (n,3) and [1] triangle centroids
+            (n,3)
+    """
     normals = []
     centroids = []
     for tri in simplices:
-        normal = np.cross(points[tri[1]] - points[tri[0]],
-                          points[tri[2]] - points[tri[0]])
+        normal = np.cross(vertices[tri[1]] - vertices[tri[0]],
+                          vertices[tri[2]] - vertices[tri[0]])
         normals.append(normal)
 
-        centroid = np.average(points[tri, :], axis=0)
+        centroid = np.average(vertices[tri, :], axis=0)
         centroids.append(centroid)
 
     return np.array(normals), np.array(centroids)
 
 
-def alpha_shape(points, alpha, only_outer=True):
-    """
-    Compute the alpha shape (concave hull) of a set of points.
-    :param points: np.array of shape (n,2) points.
-    :param alpha: alpha value.
-    :param only_outer: boolean value to specify if we keep only the outer border or also inner edges.
-    :return: set of (i,j) pairs representing edges of the alpha-shape. (i,j) are the indices in the points array.
+def alpha_shape(points: Array[float, ..., 2], alpha: float,
+                only_outer=True) -> Array[float, ..., 2]:
+    """Compute the alpha shape (concave hull) of a set of points.
 
-    source: https://stackoverflow.com/a/51082039/8040299
+    Args:
+        points (Array): Array (n, 2) of points.
+        alpha (float): Alpha shape value.
+        only_outer (bool, optional): Toggle if to keep only outer border or
+            also inner edges. Defaults to True.
 
+    Returns:
+        Array: set of (i,j) pairs representing edges of the alpha-shape. (i,j)
+        are the indices in the points array.
+
+    Source:
+        https://stackoverflow.com/a/51082039/8040299
     """
     from scipy.spatial import Delaunay
 
@@ -169,9 +191,10 @@ def alpha_shape(points, alpha, only_outer=True):
         """
         if (i, j) in edges or (j, i) in edges:
             # already added
-            assert (j, i) in edges, "Can't go twice over same directed edge right?"
+            assert (
+                j, i) in edges, "Can't go twice over same directed edge right?"
             if only_outer:
-                # if both neighboring triangles are in shape, it's not a boundary edge
+                # if both neighboring triangles in shape, it's not a boundary edge
                 edges.remove((j, i))
             return
         edges.add((i, j))
@@ -186,9 +209,9 @@ def alpha_shape(points, alpha, only_outer=True):
         pc = points[ic]
         # Computing radius of triangle circumcircle
         # www.mathalino.com/reviewer/derivation-of-formulas/derivation-of-formula-for-radius-of-circumcircle
-        a = np.sqrt((pa[0] - pb[0]) ** 2 + (pa[1] - pb[1]) ** 2)
-        b = np.sqrt((pb[0] - pc[0]) ** 2 + (pb[1] - pc[1]) ** 2)
-        c = np.sqrt((pc[0] - pa[0]) ** 2 + (pc[1] - pa[1]) ** 2)
+        a = np.sqrt((pa[0] - pb[0])**2 + (pa[1] - pb[1])**2)
+        b = np.sqrt((pb[0] - pc[0])**2 + (pb[1] - pc[1])**2)
+        c = np.sqrt((pc[0] - pa[0])**2 + (pc[1] - pa[1])**2)
         s = (a + b + c) / 2.0
         area = np.sqrt(s * (s - a) * (s - b) * (s - c))
         circum_r = a * b * c / (4.0 * area)
@@ -199,12 +222,16 @@ def alpha_shape(points, alpha, only_outer=True):
     return edges
 
 
-def is_inside(x, y, points, edges, eps=1.0e-10):
+def is_inside(x: float,
+              y: float,
+              points: Array,
+              edges: Array,
+              eps: float = 1.0e-10):
     """source: https://stackoverflow.com/a/51082039/8040299"""
     intersection_counter = 0
     for i, j in edges:
         # assert abs((points[i, 1] - y) * (points[j, 1] - y)) > eps, 'Need to handle these end cases separately'
-        y_in_edge_domain = ((points[i, 1] - y) * (points[j, 1] - y) < 0)
+        y_in_edge_domain = (points[i, 1] - y) * (points[j, 1] - y) < 0
         if y_in_edge_domain:
             upper_ind, lower_ind = (i, j) if (points[i, 1] - y) > 0 else (j, i)
             upper_x = points[upper_ind, 0]
@@ -213,19 +240,37 @@ def is_inside(x, y, points, edges, eps=1.0e-10):
             lower_y = points[lower_ind, 1]
 
             # is_left_turn predicate is evaluated with: sign(cross_product(upper-lower, p-lower))
-            cross_prod = (upper_x - lower_x) * (y - lower_y) - (upper_y - lower_y) * (x - lower_x)
-            assert abs(cross_prod) > eps, 'Need to handle these end cases separately'
-            point_is_left_of_segment = (cross_prod > 0.0)
+            cross_prod = (upper_x - lower_x) * (y - lower_y) - (
+                upper_y - lower_y) * (x - lower_x)
+            assert abs(
+                cross_prod) > eps, "Need to handle these end cases separately"
+            point_is_left_of_segment = cross_prod > 0.0
             if point_is_left_of_segment:
                 intersection_counter = intersection_counter + 1
     return (intersection_counter % 2) != 0
 
 
-def gen_surf(points, alphagit):
+def triangulate_surf(points: Array[float, ..., 3], alpha: float):
+    """Triangulate surface using Delaunay triangulation
+    
+    Args:
+        points (Array[float, ..., 3]): Points to be triangulated.
+        alpha (float): Alpha shape parameter.
+    
+    Returns:
+        tuple: Vertices Array[float, ..., 3], Simplices Array[int, ..., 3]
+    """
     from scipy.spatial import Delaunay
-    tri = Delaunay(points[:,:2])
-    tri.normals, tri.centroids = get_orient(tri.simplices, points)
-    edges = alpha_shape(points[:,:2], alpha)
-    filter_ = np.array([is_inside(c[0], c[1], points[:,:2], edges) for c in tri.centroids])
-#     return points, tri.points, tri.simplices[filter_]
-    return np.concatenate((tri.points, points[:,2][:,np.newaxis]), axis=1), tri.simplices[filter_]
+
+    tri = Delaunay(points[:, :2])
+    tri.normals, tri.centroids = get_orient(points, tri.simplices)
+    if alpha is not None:
+        edges = alpha_shape(points[:, :2], alpha)
+        filter_ = np.array([
+            is_inside(c[0], c[1], points[:, :2], edges) for c in tri.centroids
+        ])
+    #     return points, tri.points, tri.simplices[filter_]
+    return (
+        np.concatenate((tri.points, points[:, 2][:, np.newaxis]), axis=1),
+        tri.simplices[filter_],
+    )
